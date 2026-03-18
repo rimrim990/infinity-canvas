c58ea74da649cff2c24ca50b58db45097a7a1303
rimrim990 <lim990718@naver.com>
2026-03-19 00:26:37 +0900

    feat: integrate canvas area with toolbar state and define canvas element types

--- src/entities/editor/model/types.ts
+++ src/entities/editor/model/types.ts
@@ -1,3 +1,36 @@
+type CanvasElementType = 'rectangle' | 'ellipse' | 'text' | 'connector'
+
+/**
+ * Canvas
+ */
+export interface CanvasElement {
+    type: CanvasElementType,
+    name: string,
+    isVisible: boolean,
+    position: Position,
+    size: Size,
+    style: Style
+}
+
+export interface CanvasRectangle extends CanvasElement {
+    type: 'rectangle'
+}
+
+type Size = {
+    width: number
+    height: number
+}
+
+type Style = {
+    fill: string
+    stroke: string
+}
+
+type Position = {
+    x: number
+    y: number
+}
+
 /**
  * Toolbar
  */
--- src/features/editor/ui/canvas-area.tsx
+++ src/features/editor/ui/canvas-area.tsx
@@ -1,4 +1,12 @@
+import {useRef} from "react";
+import {useAtomValue} from "jotai";
+import {toolbarAtom} from "@/entities/editor/model/toolbar.ts";
+import {cn} from "@/shared/lib/utils.ts";
+
 export default function CanvasArea() {
+    const toolbarState = useAtomValue(toolbarAtom)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+
     return (
         <div className="flex-1 relative overflow-hidden bg-neutral-100">
             {/* grid background */}
@@ -13,8 +21,12 @@ export default function CanvasArea() {
 
             {/* canvas */}
             <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-[900px] h-[600px] bg-white shadow-lg border rounded-lg flex items-center justify-center text-sm text-muted-foreground">
-                    Canvas Placeholder
+                <div
+                    className="w-[900px] h-[600px] bg-white shadow-lg border rounded-lg flex items-center justify-center text-sm text-muted-foreground">
+                    <canvas id='canvas-area' ref={canvasRef}
+                            className={cn('w-full h-full', {'cursor-crosshair': toolbarState === 'rectangle'})}>
+                        Your browser does not support the HTML canvas tag.
+                    </canvas>
                 </div>
             </div>
         </div>
